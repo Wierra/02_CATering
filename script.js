4fedc681edd68efa866dfed444cb4261f78b5915
Věra Juračáková <juracakova.vera@gmail.com>
2024-11-16 00:31:08 +0100

    Řešení úkolu verze 02

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-const btnOver = document.querySelector("#butDost")
+const btnAft = document.querySelector("#btnBef")
+const text = document.querySelector("#selection")
+
 
 setTimeout (() => {
-    btnOver.textContent = "Nabídka pro vás :)";
-    btnOver.disabled = false;
+    btnAft.textContent = "Nabídka pro vás :)";
+    btnAft.classList.add ('btnNew');
+    btnAft.disabled = false;
+    text.textContent = "HURÁ, našli jsme...";
 }, 4000)
 
 const justFood = (pocetLidi) => {
-    return `Catering Just Food pro ${pocetLidi} za ${(pocetLidi * 300)} Kč.`
+    return `Catering Just Food pro ${pocetLidi} za ${(pocetLidi * 300).toLocaleString('cz')} Kč.`
 }
 
 const yourMama = (pocetLidi) => {
-    return `Catering Your Mama pro ${pocetLidi} za ${(pocetLidi * 600)} Kč.`
+    return `Catering Your Mama pro ${pocetLidi} za ${(pocetLidi * 600).toLocaleString('cz')} Kč.`
 }
 
 const flavourHaven =  (pocetLidi) => {
-    return `Catering Flavour Haven pro ${pocetLidi} za ${(pocetLidi * 900)} Kč.`
+    return `Catering Flavour Haven pro ${pocetLidi} za ${(pocetLidi * 900).toLocaleString('cz')} Kč.`
 }
 
 console.log(justFood(100))
@@ -32,15 +36,15 @@ createEvent("Galavečer SVĚTluška", 100, yourMama)
 
 setTimeout (() => {
     const food = document.querySelector('#food')
-    food.textContent = "Festival smíchu & pláče s cateringem od Just Food pro 100 osob za " + (100*300) + " Kč."
+    food.textContent = "Festival smíchu & pláče s cateringem od Just Food pro 100 osob za " + (100*300).toLocaleString('cz') + " Kč."
 }, 5000)
 
 setTimeout (() => {
     const mama = document.querySelector('#mama')
-    mama.textContent = "Filmový festival s cateringem od Your Mamam pro 100 osob za " + (100*600) + " Kč."
+    mama.textContent = "Filmový festival s cateringem od Your Mamam pro 100 osob za " + (100*600).toLocaleString('cz') + " Kč."
 }, 6000)
 
 setTimeout (() => {
     const haven = document.querySelector('#haven')
-    haven.textContent = "Galavečer SVĚTluška s cateringem od Flavour Haven pro 100 osob za " + (100*900) + " Kč."
+    haven.textContent = "Galavečer SVĚTluška s cateringem od Flavour Haven pro 100 osob za " + (100*900).toLocaleString('cz') + " Kč."
 }, 7000)
