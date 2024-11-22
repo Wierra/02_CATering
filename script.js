6a025379b575b76166898feea3a4eb2b27ab3b69
Věra Juračáková <juracakova.vera@gmail.com>
2024-11-22 16:19:39 +0100

    Řešení úkolu verze 02_22.11.2024

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btnAft = document.querySelector("#btnBef")
 const text = document.querySelector("#selection")
+const dot = document.querySelector("#food")
 
 
 setTimeout (() => {
@@ -12,11 +13,9 @@ setTimeout (() => {
 const justFood = (pocetLidi) => {
     return `Catering Just Food pro ${pocetLidi} za ${(pocetLidi * 300).toLocaleString('cz')} Kč.`
 }
-
 const yourMama = (pocetLidi) => {
     return `Catering Your Mama pro ${pocetLidi} za ${(pocetLidi * 600).toLocaleString('cz')} Kč.`
 }
-
 const flavourHaven =  (pocetLidi) => {
     return `Catering Flavour Haven pro ${pocetLidi} za ${(pocetLidi * 900).toLocaleString('cz')} Kč.`
 }
@@ -34,17 +33,32 @@ createEvent("Filmový festival", 100, flavourHaven)
 createEvent("Galavečer SVĚTluška", 100, yourMama)
 
 
+btnAft.addEventListener("click", () => {
+    btnAft.classList.add ('submitClick');
+})
+
 setTimeout (() => {
     const food = document.querySelector('#food')
     food.textContent = "Festival smíchu & pláče s cateringem od Just Food pro 100 osob za " + (100*300).toLocaleString('cz') + " Kč."
-}, 5000)
+}, 7000)
 
 setTimeout (() => {
     const mama = document.querySelector('#mama')
     mama.textContent = "Filmový festival s cateringem od Your Mamam pro 100 osob za " + (100*600).toLocaleString('cz') + " Kč."
-}, 6000)
+}, 8000)
 
 setTimeout (() => {
     const haven = document.querySelector('#haven')
     haven.textContent = "Galavečer SVĚTluška s cateringem od Flavour Haven pro 100 osob za " + (100*900).toLocaleString('cz') + " Kč."
-}, 7000)
+}, 9000)
+
+const butSubmit = document.querySelector("#submit")
+setTimeout (() => {
+    butSubmit.textContent = "TO CHCI!";
+    butSubmit.classList.add ('submitNew');
+}, 10000)
+
+butSubmit.addEventListener("click", () => {
+    butSubmit.textContent = "Skvělá volba - určitě si pochutnáš :)";
+    butSubmit.classList.add ('submitClick');
+})
